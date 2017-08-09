29dccf3bd6ed6a6ce45d03ba425cd0225f5c3c22
mmallo5 <mmallo@us.ibm.com>
2017-08-09 16:34:56 -0500

    remove whitespace

--- index.php
+++ index.php
@@ -105,7 +105,6 @@ function orderItem(itemID){
 	<a href="./autoLoadTest.html">Catalog Load Tester</a>
 
 	
-	
      <!-- Add large images, uncomment the next img lines to create a response time alarm in BAM -->
 	<!--
 	<img id="berlin" src="http://upload.wikimedia.org/wikipedia/commons/2/26/Invalidenpark%2C_Berlin-Mitte%2C_360x180%2C_160326%2C_ako.jpg" height="25%" width="25%" alt="Oops">
